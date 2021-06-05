53ae58655665b0faa6318261de6f9d55ca45bc6d
FirstMste <84036016+FirstMste@users.noreply.github.com>
2021-06-05 18:38:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 Clicked.addEventListener("click", addList );
-Clicked.addEventListener("click", clearInput)
+Red.addEventListener("click", red);
+Green.addEventListener("click", green);
+Blue.addEventListener("click", blue);
+Reset.addEventListener("click", reset);
 
+
+function reset() {
+    var backing = document.getElementById("backing");
+
+    backing.style.background = "black"
+
+
+}
+
+
+function red(){
+    var backing = document.getElementById("backing");
+
+   backing.style.background = "red"
+
+}
+function green(){
+    var backing = document.getElementById("backing");
+
+   backing.style.background = "green"
+
+}
+function blue(){
+    var backing = document.getElementById("backing");
+
+   backing.style.background = "blue"
+
+}
 
 function addList(){
     var user = document.getElementById("user").value, 
     listNode =  document.getElementById("list"), 
     listli = document.createElement("LI"), 
-    txtNode = document.createTextNode(user);
+    txtNode = document.createTextNode(user),
+    btnCr = document.createElement("BUTTON"),
+    btnTxt = document.createTextNode("delete");
 
     listli.appendChild(txtNode);
     listNode.appendChild(listli);
+    btnCr.appendChild(btnTxt);
 }
-
-function clearInput() {
-    
-    
-}
-    
-
-
-
-    
